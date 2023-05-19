92445e30ce43c262c4eaee5efa031509389971b6
mr34 <allaberdi16yazhanow@gmail.com>
2023-05-19 09:40:17 +0500

    last edit

--- api/account/utils.py
+++ api/account/utils.py
@@ -2,12 +2,14 @@ import requests
 import random
 from account.models import CustomUser
 
+# SMS API 
+url = "https://mobizon.kz"
 
-def send_otp(username):
+def send_otp(username):# will send otp verification code when user register
     otp = random.randint(1000, 9999)
     user = CustomUser.objects.get(username=username)
     user.otp = otp
-    apikey = kz35cd136b7165dd5795029af1191ace1ed868584437d7d548f4d4a232bfc761644da0
+    apikey = ""
     user.save()
     reqUrl = "https://api.mobizon.kz/service/message/sendsmsmessage?recipient={username}&text={otp}&apiKey={apikey}"
     headersList = {
@@ -15,20 +17,19 @@ def send_otp(username):
         "User-Agent": "Thunder Client (https://www.thunderclient.com)"
     }
     payload = ""
-    # response = requests.request(
-        # "POST", reqUrl, data=payload,  headers=headersList)
-    print(otp)
+    response = requests.request(
+        "POST", reqUrl, data=payload,  headers=headersList)
 
 
-def password_reset_otp(otp, username):
-    apikey = kz35cd136b7165dd5795029af1191ace1ed868584437d7d548f4d4a232bfc761644da0
+
+def password_reset_otp(otp, username): #send otp verification code when user wants reset password
+    apikey = ""
     reqUrl = "https://api.mobizon.kz/service/message/sendsmsmessage?recipient={username}&text={otp}&apiKey={apikey}"
     headersList = {
         "Accept": "*/*",
         "User-Agent": "Thunder Client (https://www.thunderclient.com)"
     }
     payload = ""
-    # response = requests.request(
-        # "POST", reqUrl, data=payload,  headers=headersList)
-    print(otp)
+    response = requests.request(
+        "POST", reqUrl, data=payload,  headers=headersList)
 
